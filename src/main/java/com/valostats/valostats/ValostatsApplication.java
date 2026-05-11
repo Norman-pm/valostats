c8aa109df2983d927b8091f6b029e0bd58de6a70
Norman-pm <normanperezmalanda@tarragona.lasalle.cat>
2026-05-11 12:30:45 +0200

    init proyecto con spring boot amb dependencies base

---
+++ src/main/java/com/valostats/valostats/ValostatsApplication.java
@@ -0,0 +1,13 @@
+package com.valostats.valostats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValostatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValostatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valostats/valostats/ValostatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valostats.valostats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValostatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

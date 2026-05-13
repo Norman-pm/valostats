d53787263232867125cc827746103322b309e008
Norman-pm <normanperezmalanda@tarragona.lasalle.cat>
2026-05-13 11:11:53 +0200

    Corregidos un par de errores y clase Stats casi terminada

--- src/main/java/com/valostats/valostats/model/Jugador.java
+++ src/main/java/com/valostats/valostats/model/Jugador.java
@@ -1,7 +1,7 @@
 package com.valostats.valostats.model;
 
 import jakarta.persistence.*;
-import java.util.List;
+
 
 @Entity
 @Table(name = "jugadors")
--- src/main/java/com/valostats/valostats/model/Stats.java
+++ src/main/java/com/valostats/valostats/model/Stats.java
@@ -1,4 +1,66 @@
 package com.valostats.valostats.model;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "stats")
 
 public class Stats {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Kills", nullable = false)
+    private int kills;
+
+    @Column (name = "Deaths", nullable = false)
+    private int deaths;
+
+    @Column (name = "Assists", nullable = false)
+    private int assists;
+
+    @Column (name = "Rondas", nullable = false)
+    private int rondas;
+
+    @Column (name = "K:D", nullable = false)
+    private double kd;
+
+    @Column (name = "Hs%", nullable = false)
+    private double hsPorcentaje;
+
+    @Column (name = "Rating", nullable = false)
+    private double rating;
+
+    @Column (name = "torneo", nullable = false, length = 100)
+    private String torneo;
+
+    @OneToOne
+    @JoinColumn (name = "jugador_id", nullable = false)
+    private Jugador jugador;
+
+    public Stats(){}
+
+    public Stats(int kills, int deaths, int assists, int rondas, double kd,double hsPorcentaje, double rating, String torneo, Jugador jugador){
+        this.kills = kills;
+        this.deaths = deaths;
+        this.assists = assists;
+        this.rondas = rondas;
+        this.kd = kd;
+        this.hsPorcentaje = hsPorcentaje;
+        this.rating = rating;
+        this.torneo = torneo;
+        this.jugador = jugador;
+    }
+
+    public Long getId(){ return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public int getkills(){ return kills; }
+    public void setkills(int kills){ this.kills = kills; }
+
+    public int getDeaths() { return deaths; }
+    public void setDeaths(int deaths){ this.deaths = deaths; }
+
+    public int getAssists
+
+
 }
